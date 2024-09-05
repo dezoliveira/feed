f84d899fd79d9b972288cfe7af2a0e378c2bcb18
Andres Oliveira <andresoliveira@protonmail.com>
2024-09-05 18:25:07 -0300

    add: SUSE font

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,17 @@
 </script>
 
 <template>
-  <header>
+  <div>
     <nav class="navbar">
       <RouterLink to="/">Home</RouterLink>
       <RouterLink to="/feed">Feed</RouterLink>
       <RouterLink to="/register">Register</RouterLink>
       <RouterLink to="/sign-in">SignIn</RouterLink>
     </nav>
-  </header>
-  <RouterView />
+    <RouterView />
+  </div>
 </template>
 
 <style scoped>
-  .navbar {
-    position: absolute;
-    top: 0;
-    left: 0;
-    padding: 15px;
-  }
+
 </style>
