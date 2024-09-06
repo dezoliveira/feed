acc35c9c0ef333e092a3fd9e086ef13a350243e8
Andres Oliveira <andresoliveira@protonmail.com>
2024-09-05 23:02:27 -0300

    add: SignOut user

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,44 @@
-<script setup>
-</script>
-
 <template>
   <div>
     <nav class="navbar">
       <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/feed">Feed</RouterLink>
-      <RouterLink to="/register">Register</RouterLink>
-      <RouterLink to="/sign-in">SignIn</RouterLink>
+      <span v-if="isLoggedIn">
+        <RouterLink to="/feed">Feed</RouterLink>
+      </span>
+      <span v-if="isLoggedIn">
+       <button @click="logOut">Logout</button> 
+      </span>
+      <span v-else>
+        <RouterLink to="/register">Register</RouterLink>
+        <RouterLink to="/sign-in">SignIn</RouterLink>
+      </span>
     </nav>
     <RouterView />
   </div>
 </template>
+<script setup>
+  import { ref, watchEffect } from 'vue'
+  import { onAuthStateChanged, signOut } from 'firebase/auth';
+  import { useRouter } from 'vue-router';
 
+  const isLoggedIn = ref(true)
+  const auth = signOut()
+  const router = useRouter()
+
+  // runs after firebase is initialized
+  onAuthStateChanged(auth, function(user) {
+    if (user) {
+      isLoggedIn.value = true
+    } else {
+      isLoggedIn.value = false
+    }
+  })
+
+  const logOut = async () => {
+    await signOut()
+    router.push('/')
+  }
+</script>
 <style scoped>
 
 </style>
