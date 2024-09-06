b4224598bc6690375fcf408635b870a3b690a14b
Andres Oliveira <andresoliveira@protonmail.com>
2024-09-05 22:08:48 -0300

    add: Validate error message w/ cases

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -4,6 +4,7 @@
       <h1>Signup with login</h1>
       <input type="text" placeholder="Email" v-model="email" />
       <input type="password" placeholder="Password" v-model="password" />
+      <p v-show="errorMessage">{{ errorMessage }}</p>
       <button>Submit</button>
     </form>
   </div>
@@ -15,9 +16,31 @@
 
   const email = ref('')
   const password = ref('')
+  const errorMessage = ref('')
 
   const auth = getAuth()
   const router = useRouter()
+
+  const validateError = (error) => {
+    console.log(error)
+    switch (error.code) {
+      case 'auth/invalid-email':
+        errorMessage.value = 'Invalid email'
+        break
+
+      case 'auth/user-not-found':
+        errorMessage.value = 'No account with that email was found'
+        break
+
+      case 'auth/wrong-password':
+        errorMessage.value = 'Incorrect password'
+        break
+
+      default:
+        errorMessage.value = 'Email or password was incorrect'
+        break
+    }
+  }
   
   const signUp = async() => {
     await signInWithEmailAndPassword(
@@ -31,8 +54,8 @@
       router.push('/feed')
     })
 
-    .catch((error) => {
-      console.log(error.code)
+    .catch(error => {
+      validateError(error.code)
     })
   }
 
