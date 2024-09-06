f28f01c9178917f4508e1314ac67081a6123e7f7
Andres Oliveira <andresoliveira@protonmail.com>
2024-09-06 20:31:36 -0300

    style: navbar styles

--- src/App.vue
+++ src/App.vue
@@ -5,20 +5,34 @@
       <span v-if="isLoggedIn">
         <RouterLink to="/feed">Feed</RouterLink>
       </span>
-      <span v-if="isLoggedIn">
-       <button @click="logOut">Logout</button> 
-      </span>
-      <span v-else>
-        <RouterLink to="/register">Register</RouterLink>
-        <RouterLink to="/sign-in">SignIn</RouterLink>
-      </span>
+      <div class="auth-buttons">
+        <span v-if="isLoggedIn">
+         <button @click="logOut">Logout</button> 
+        </span>
+        <span v-else>
+          <span>
+            <RouterLink to="/register">
+              <button class="button primary">
+                Register
+              </button>
+            </RouterLink>
+          </span>
+          <span>
+            <RouterLink to="/sign-in">
+              <button class="button secondary">
+                SignIn
+              </button>
+            </RouterLink>
+          </span>
+        </span>
+      </div>
     </nav>
     <RouterView />
   </div>
 </template>
 <script setup>
   import { ref, watchEffect } from 'vue'
-  import { getAuth, onAuthStateChanged, signOut } from 'firebase/auth';
+  import { getAuth, onAuthStateChanged } from 'firebase/auth';
   import { useRouter } from 'vue-router';
 
   const isLoggedIn = ref(true)
@@ -43,5 +57,40 @@
   }
 </script>
 <style scoped>
+  .navbar {
+    @apply
+      flex
+      items-center
+      justify-between
+      gap-4
+      bg-slate-900 
+      px-[24px]
+      py-[10px]
+      text-slate-50
+      w-full
+      h-[20%]
+  }
 
+  .auth-buttons span {
+    @apply 
+      flex
+      gap-2
+  }
+
+  .button {
+    @apply 
+      px-[10px]
+      py-[4px]
+      bg-slate-50
+  }
+
+  .primary {
+    @apply
+      bg-orange-500
+  }
+
+  .secondary {
+    @apply
+      bg-cyan-500
+  }
 </style>
