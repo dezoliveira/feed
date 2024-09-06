8d4a57b32a6c7de892c1ddc3bafb87d759b23ae0
Andres Oliveira <andresoliveira@protonmail.com>
2024-09-05 21:30:51 -0300

    add: register on firebase

--- src/main.js
+++ src/main.js
@@ -4,8 +4,23 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+	apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+	authDomain: "vue-auth-abafa.firebaseapp.com",
+	databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+	projectId: "vue-auth-abafa",
+	storageBucket: "vue-auth-abafa.appspot.com",
+	messagingSenderId: "692760090351",
+	appId: "1:692760090351:web:bf0aa13a9d9554a667ad1a"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
 const app = createApp(App)
 
 app.use(router)
 
 app.mount('#app')
+
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,9 +1,44 @@
-<script setup>
-</script>
 <template>
   <div class="register">
-    <h1>Register</h1>
+    <form class="form" @submit.prevent="register">
+      <h1>Create an Account</h1>
+      <input type="text" placeholder="Email" v-model="email" />
+      <input type="password" placeholder="Password" v-model="password" />
+      <button>Submit</button>
+    </form>
   </div>
 </template>
-<style>
+<script setup>
+  import { ref } from 'vue'
+  import { useRouter } from 'vue-router';
+  import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth'
+
+  const email = ref('')
+  const password = ref('')
+
+  const auth = getAuth()
+  const router = useRouter()
+
+  const register = async () => {
+    await createUserWithEmailAndPassword(
+      auth,
+      email.value,
+      password.value
+    )
+
+    .then((data) => {
+      console.log('Successfully registered!');
+      router.push('/feed') // redirect to the feed
+    })
+    
+    .catch(error => {
+      console.log(error.code)
+    })
+  }
+</script>
+<style scoped>
+  .form {
+    display: flex;
+    flex-direction: column;
+  }
 </style>
