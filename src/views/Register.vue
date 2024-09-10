afac9e60cd1c43b44160db65e2c4c9fed091c7da
Andres Oliveira <andresoliveira@protonmail.com>
2024-09-10 17:50:57 -0300

    add: Register validations before post

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -6,38 +6,121 @@
       </div>
       <form class="form" @submit.prevent="register">
         <input type="text" placeholder="Email" v-model="email" />
+        <span class="text-red-500" v-if="formData.email.error">{{ formData.email.message }}</span>
         <input type="password" placeholder="Password" v-model="password" />
+        <span class="text-red-500" v-if="formData.password.error">{{ formData.password.message }}</span>
+        <p v-show="errorMessage" class="errorMessage">{{ errorMessage }}</p>
         <button class="button success">Submit</button>
       </form>
     </div>
   </div>
 </template>
 <script setup>
-  import { ref } from 'vue'
+  import { reactive, ref } from 'vue'
   import { useRouter } from 'vue-router';
   import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth'
 
   const email = ref('')
   const password = ref('')
+  const errorMessage = ref('')
+  
+  const formData = reactive({
+    'email': {
+      'error': false,
+      'message': false
+    },
+
+    'password': {
+      'error': false,
+      'message': false
+    }
+  })
 
   const auth = getAuth()
   const router = useRouter()
 
-  const register = async () => {
-    await createUserWithEmailAndPassword(
-      auth,
-      email.value,
-      password.value
-    )
-
-    .then((data) => {
-      console.log('Successfully registered!');
-      router.push('/sign-in') // redirect to the feed
-    })
+  const emailValidate = () => {
+    errorMessage.value = ''
+    const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
     
-    .catch(error => {
-      console.log(error.code)
-    })
+    formData.email.error = false
+    formData.email.message = ''
+
+    if (email.value.length <= 0) {
+      formData.email.error = true
+      formData.email.message = 'Email cant be null'
+
+      return false
+    }
+
+    if (!emailRegex.test(email.value)){
+      formData.email.error = true
+      formData.email.message = 'Email is Invalid'
+
+      return false
+    }
+
+    return true
+  }
+
+  const passwordValidate = () => {
+    errorMessage.value = ''
+    formData.password.error = false
+    formData.password.message = ''
+    
+    if (password.value.length < 8) {
+      console.log(password.value.length)
+      formData.password.error = true
+      formData.password.message = 'Password have to be more than 8 characteres'
+
+      return false
+    }
+
+    return true
+  }
+
+  const formValidate = () => {
+    if (emailValidate() && passwordValidate()) {
+      return true
+    }
+
+    return false
+  }
+
+  const validateError = (error) => {
+    console.log(error)
+    switch (error) {
+      case 'auth/email-already-in-use':
+        errorMessage.value = 'Email already in use!'
+        break
+
+      default:
+      errorMessage.value = 'Email or password was incorrect'
+      
+      break
+    }
+  }
+
+  const register = async () => {
+    const isValid = formValidate()
+    if (isValid) {
+      await createUserWithEmailAndPassword(
+        auth,
+        email.value,
+        password.value
+      )
+  
+      .then((data) => {
+        console.log('Successfully registered!');
+        router.push('/feed') // redirect to the feed
+      })
+      
+      .catch(error => {
+        console.log('error')
+        validateError(error.code)
+      })
+    }
+
   }
 </script>
 <style scoped>
